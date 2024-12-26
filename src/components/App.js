6bfe829bd95b71a222d8cbd7918ad7b60c66e0d9
Anurag Joshi <anuragj990@gmail.com>
2024-12-26 19:57:15 +0530

    1st try

--- src/components/App.js
+++ src/components/App.js
@@ -32,10 +32,14 @@ const App = () => {
     { name: 'Mount Abu', country: 'India' },
     { name: 'Tirupati', country: 'India' },
     ]
+
+    let filteredcities=cityList.filter((city)=> city.country==='India');
     
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol>
+       {filteredcities.map((city,index)=> <li key={`location${index+1}`}>{city.name}</li>)}
+       </ol>    
     </div>
   )
 }
